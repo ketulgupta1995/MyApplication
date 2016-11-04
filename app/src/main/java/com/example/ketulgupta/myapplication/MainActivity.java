abf23b908a0c5721ae928c9f063f9db1e8a4d9c5
Ketul Gupta <ketulgupta1995@gmail.com>
2016-11-04 13:53:00 +0530

    "simple use of accelerometer not caliberated to one step for one count"

--- app/src/main/java/com/example/ketulgupta/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ketulgupta/myapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ketulgupta.myapplication;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +14,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+    int  count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+//        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+//        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void startit(View view) {
+        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void endit(View view) {
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+//        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +85,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            long currtime= System.currentTimeMillis();
+            if((currtime-lastUpdate)>100)
+            {
+                long diff=(currtime-lastUpdate);
+                lastUpdate=currtime;
+                float speed=Math.abs(x+y+z-last_x-last_y-last_z)/diff*10000;
+                if(speed>SHAKE_THRESHOLD)
+                {
+                    EditText editText=(EditText)findViewById(R.id.editText);
+                    editText.setText(Integer.toString(count));
+                    count++;
+                }
+                last_x=x;
+                last_y=y;
+                last_z=z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
